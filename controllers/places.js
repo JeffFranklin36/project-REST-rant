e4a6bf91e95da411a90677cabf4293417de3a9d1
Jeffrey <jeff.1001@live.com>
2022-05-18 17:01:07 -0500

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,33 @@
 const router = require('express').Router()
 
+
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 // GET /places
-router.get('/', (req, res) => {
-     let places = [{
-          name: 'H-Thai-ML',
-          city: 'Seattle',
-          state: 'WA',
-          cuisines: 'Thai, Pan-Asian',
-          pic: "/images/thai-food-img.avif"
-        }, {
-          name: 'Coding Cat Cafe',
-          city: 'Phoenix',
-          state: 'AZ',
-          cuisines: 'Coffee, Bakery',
-          pic: "/images/coffee-cat-img.avif"
-        }]        
+router.get('/', (req, res) => { 
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: "/images/thai-food-img.avif"
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: "/images/coffee-cat-img.avif"
+  }]       
      res.render('places/index', { places })
+})
+
+router.post('/', (req, res) => {
+//   if (!req.body.pic) {
+//    req.body.pic = '/images/default-restaraunt-img.avif'
+//  }
+// places.push(req.body)
+res.send('/places')
 })
 
 module.exports = router
